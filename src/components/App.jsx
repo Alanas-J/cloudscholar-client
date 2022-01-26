37d62598c81651f343d714214ed980779bd87981
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-01-26 19:22:37 +0000

    saving work, will scrap later

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,13 @@ import MainDisplay from './main_display/MainDisplay';
 import LoginDisplay from './login_display/LoginDisplay';
 import './App.css';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 function App() {
   const [loggedIn, setLoggedIn] = useState(false); // will be used as the hook to dictate if logged in.
+
+  const display = useSelector(state => state.appDisplay.value);
+  const dispatch = useDispatch();
 
 
   return (
@@ -15,5 +20,22 @@ function App() {
     </div>
   );
 }
-
 export default App;
+
+
+// Used to pick what to display.
+function renderSwitch(display){
+  switch(display) {
+    case 'login':
+      return <LoginDisplay/>;
+
+    case 'main_display':
+      return <LoginDisplay/>;  
+    
+      
+    default:
+      return <LoginDisplay/>;
+  }
+
+}
+
