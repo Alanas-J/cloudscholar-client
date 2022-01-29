a889570aa169a04676456bbe53f5243b7ece98b3
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-01-29 14:38:12 +0000

    added some design changes to home.

--- src/components/main_display/home/Home.jsx
+++ src/components/main_display/home/Home.jsx
@@ -4,25 +4,54 @@ function Home() {
       <div className='home rounded-left d-flex flex-fill justify-content-between'>
       
         <section className='flex-fill'>
-        <h1 className='border-bottom p-3'>Welcome User</h1>
 
-        <div className=''>
-          <div className='scheduleComponent mx-4'>
-            <h2 className='border-bottom p-2 '>Today's Schedule</h2>
-            <div className='mx-4'> display will go here</div>
+        <div className='row pt-2'>
+          <div className="col">
+            <div className='scheduleComponent mx-4'>
+              <div className="row border-bottom">
+                <div className="col">
+                  <h2 className='p-2 '>Today's Classes</h2>
+                </div>
+                
+                <div className="col-1 d-flex justify-content-end"><button type="button" class="btn btn-primary">+</button></div>
+              </div>
+              <div className='mx-4'> display will go here</div>
+            </div>
           </div>
 
-          <div className='taskComponent mx-4'>
-            <h2 className='border-bottom p-2 '> Additional Upcoming Tasks</h2>
-            <div className='mx-4'> display will go here</div>
-          </div>
 
+          <div className="col">
+            <div className='scheduleComponent mx-4'>
+              <div className="row border-bottom">
+                <div className="col">
+                  <h2 className='p-2 '>Upcoming Tasks</h2>
+                </div>
+                
+                <div className="col-1 d-flex justify-content-end"><button type="button" class="btn btn-primary">+</button></div>
+              </div>
+              <div className='mx-4'> display will go here</div>
+            </div>
+          </div>
+          
+
+          
         
         </div>
         </section>
 
         <section className='shortcuts p-4 border border-top-0'>
-          <h4 className='border-bottom'>Quick Shortcut URLs</h4>
+          <div className="row">
+            <div className="col">
+              <h4 className='border-bottom'>Quick Shortcut URLs</h4>
+              <a href="" className='link-secondary'>Add new url...</a>
+              
+              
+            </div>
+          <div className="col-1">
+            
+          </div>
+        </div>
+          
         </section>
 
           
