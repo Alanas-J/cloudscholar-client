fe78f262ad044a2a796816f6c21ea7c922967413
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-01-29 16:31:12 +0000

    Home looks nicer now

--- src/components/main_display/home/Home.jsx
+++ src/components/main_display/home/Home.jsx
@@ -1,60 +1,65 @@
-import './Home.css'
+import styles from './Home.module.css'
+
 function Home() {
     return (
-      <div className='home rounded-left d-flex flex-fill'>
-      
-        <section className='flex-fill h-100'>
-
-        <div className='row  h-100 p-0 pt-2'>
-          <div className="col">
-            <div className='scheduleComponent mx-4'>
-              <div className="row border-bottom">
-                <div className="col">
-                  <h2 className='p-2 '>Today's Classes</h2>
-                </div>
-                
-                <div className="col-1 d-flex justify-content-end"><button type="button" class="btn btn-primary">+</button></div>
-              </div>
-              <div className='mx-4'> display will go here</div>
-            </div>
-          </div>
-
-
-          <div className="col">
-            <div className='scheduleComponent mx-4'>
-              <div className="row border-bottom">
-                <div className="col">
-                  <h2 className='p-2 '>Upcoming Tasks</h2>
-                </div>
-                
-                <div className="col-1 d-flex justify-content-end"><button type="button" class="btn btn-primary">+</button></div>
-              </div>
-              <div className='mx-4'> display will go here</div>
-            </div>
-          </div>
-          
-
-          
+      <div className={styles.home+" container-fluid border"}>
         
-        </div>
-        </section>
+        {/* Main row*/} 
+        <div className='row h-100'>
 
-        <section className='shortcuts p-4 border border-top-0'>
-          <div className="row">
-            <div className="col">
-              <h4 className='border-bottom'>Quick Shortcut URLs</h4>
-              <a href="" className='link-secondary'>Add new url...</a>
+          {/* Flex Column and Row containing Today's and Upcoming */} 
+          <div className='col h-100'>
+            <div className='row h-100'>
+
+
+              {/* Today's Classes Col*/} 
+              <div className="col-6 pt-2 mw-30 border-end">
+                <div className='scheduleComponent mx-4'>
+
+                  <div className="row border-bottom">
+                    <div className="col-12 d-flex justify-content-between">
+                      <h2 className='p-1 '>Today's Classes</h2>
+                      <button type="button" class="btn btn-primary">+</button>
+                    </div>
+                  </div>
+
+                  {/* Output Display To go Here*/} 
+                  <div className='mx-4'> display will go here</div>
+
+                </div>
+              </div>
+
+              {/* Upcoming Tasks Col*/} 
+              <div className="col-6 pt-2 border-left">
+                <div className='scheduleComponent mx-4'>
+                  <div className="row border-bottom">
+                    <div className="col-12 d-flex justify-content-between">
+                      <h2 className='p-1 '>Upcoming Tasks</h2>
+                      <button type="button" class="btn btn-primary">+</button>
+                    </div>
+                  </div>
+
+                   {/* Output Display To go Here*/} 
+                  <div className='mx-4'> display will go here</div>
+                </div>
+              </div>
               
-              
+
             </div>
-          <div className="col-1">
-            
           </div>
-        </div>
-          
-        </section>
 
-          
+          {/* Quick Shortcuts */} 
+          <div className={styles.shortcuts+" border-start"}>
+            <div className="row">
+              <div className="col">
+                <h4 className='border-bottom'>Quick Shortcut URLs</h4>
+                <a href="" className='link-secondary'>Add new url...</a>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
       </div>
 
     ); 
