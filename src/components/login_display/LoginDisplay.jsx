b73eb3a19af28b513be19d0729d2c3f066b8b9a9
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-01-30 16:31:12 +0000

    centred logindisplay

--- src/components/login_display/LoginDisplay.jsx
+++ src/components/login_display/LoginDisplay.jsx
@@ -4,16 +4,16 @@ function LoginDisplay({setLoggedIn}) {
     return (
       <div className='loginDisplay text-center'>
        
-       <main className="form-signin">
-          <form>
-            <img className="mb-4" src={require('./logo192.png')} alt="" width="72" height="57"/>
-            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+       <main className="form-signin d-flex flex-column justify-content-center">
+          <form className='pb-5'>
+          <img className="mb-4" src={require('./logo192.png')} alt="" width="72" height="57"/>
+            <h1 className="h3 mb-3 fw-normal">CloudScholar</h1>
 
-            <div className="form-floating">
+            <div className="form-floating my-1">
               <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
               <label htmlFor="floatingInput">Email address</label>
             </div>
-            <div className="form-floating">
+            <div className="form-floating my-1">
               <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
               <label htmlFor="floatingPassword">Password</label>
             </div>
@@ -25,7 +25,6 @@ function LoginDisplay({setLoggedIn}) {
             </div>
             <button className="w-100 btn btn-lg btn-primary" onClick={() => setLoggedIn(true)}>Sign in</button>
             <a>Don't have an account? Register Here</a>
-            <p className="mt-5 mb-3 text-muted">©CloudScholar 2021–2022</p>
           </form>
         </main>
         
