60e5d37ab4a228102da13e95c19b9bdcd7c47221
Alanas-J <alanasjakubauskas@gmail.com>
2022-02-07 15:35:59 +0000

    idea of task component created needs refinement
    also bug with quick URL menu dissapearing needs to be investigated

--- src/components/main_display/home/upcoming_tasks/UpcomingTasksDisplay.jsx
+++ src/components/main_display/home/upcoming_tasks/UpcomingTasksDisplay.jsx
@@ -1,9 +1,10 @@
 import styles from '../Home.module.css'
+import Task from './task/Task'
 
 function UpcomingTasksDisplay() {
     return (
         <div className={styles.height83 + " mt-4 border bg-light border-secondary"}>
-                
+                <Task/>
         </div>
     );
 }   
---
+++ src/components/main_display/home/upcoming_tasks/task/Task.jsx
@@ -0,0 +1,31 @@
+import styles from './Task.module.css'
+
+function Task() {
+    return (
+        <div className={styles.colour +" "+ styles.taskHover+" border m-1 rounded-start d-flex "}>
+            <div className={"rounded-start me-2"} style={{width:'.3rem', backgroundColor:'light-green'}}>
+                
+            </div>
+
+            <div className={styles.taskContent}>
+                <div className="d-flex justify-content-between">
+                    <div className={styles.taskTitleWidth+" pt-1"}>
+                        <h5 className={styles.textOverflow + " border-bottom p-1"}>Software Engineering 5: Assignment 2</h5>
+                    </div>
+                    <div className="me-3 ">[*]</div>
+                </div>
+                
+                <div className="w-100">
+                    <div>
+                        <p className={styles.textOverflow + " m-0"}>
+                        Due **/**/** - One line description. Adding more words afasdfasgas</p>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+    );
+}   
+
+export default Task;
