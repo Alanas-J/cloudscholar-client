29e47617ef303945254b50433f71c2346c616dc5
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-02-07 23:28:45 +0000

    task prototype with props ready

--- src/components/main_display/home/upcoming_tasks/task/Task.jsx
+++ src/components/main_display/home/upcoming_tasks/task/Task.jsx
@@ -1,16 +1,18 @@
 import styles from './Task.module.css'
 
-function Task() {
+function Task({colour, taskName, time, subject}) {
+
+
+
+
     return (
-        <div className={styles.colour +" "+ styles.task+" border m-1 rounded-start d-flex "}>
-            <div className={"rounded-start me-2"} style={{width:'.3rem', backgroundColor:'light-green'}}>
-                
-            </div>
+        <div className={ styles.task+" border border-secondary m-1 rounded d-flex "}>
+            <div className={"rounded-start me-2"} style={{width:'.45rem', backgroundColor: colour}}></div>
 
             <div className={styles.taskContent}>
                 <div className="d-flex justify-content-between">
                     <div className={styles.taskTitleWidth+" pt-1"}>
-                        <h5 className={styles.textOverflow + " border-bottom p-1"}>Assignment 2</h5>
+                        <h5 className={styles.textOverflow + " border-bottom border-secondary p-1"}>{taskName}</h5>
                     </div>
                     <div className="me-3 ">[*]</div>
                 </div>
@@ -18,7 +20,7 @@ function Task() {
                 <div className="w-100">
                     <div>
                         <p className={styles.textOverflow + " m-0"}>
-                        Due **/**/** - Software Engineering 5</p>
+                        Due {time} - {subject}</p>
                     </div>
                     
                 </div>
