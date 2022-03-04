48de3b2845eb2b87bdbb7e47ac727eaa0d2bfb26
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-03-04 15:05:47 +0000

    saving login progress

--- src/components/login_display/LoginDisplay.jsx
+++ src/components/login_display/LoginDisplay.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react';
 import './LoginDisplay.css'
 
 function LoginDisplay({setLoggedIn}) {
+
+    const [displayState, setDisplayState] = useState({
+        loginButtonEnabled: true,
+        loginButtonText: "Sign in",
+        error: false,
+        errorMessage: "",
+        submitted: false
+    });
+
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [keepUserSigned, setKeepUserSigned] = useState(false);
+
+    if(displayState.submitted){
+        if(validateEmail(email, displayState, setDisplayState)){
+            authenticate(email, password, setDisplayState)
+        }
+    }
+
     return (
       <div className='loginDisplay text-center'>
        
@@ -10,20 +30,20 @@ function LoginDisplay({setLoggedIn}) {
             <h1 className="h3 mb-3 fw-normal">CloudScholar</h1>
 
             <div className="form-floating my-1">
-              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" onChange={e => setEmail(e.target.value)}/>
               <label htmlFor="floatingInput">Email address</label>
             </div>
             <div className="form-floating my-1">
-              <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+              <input type="password" className="form-control" id="floatingPassword" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
               <label htmlFor="floatingPassword">Password</label>
             </div>
 
             <div className="checkbox mb-3">
               <label>
-                <input type="checkbox" value="remember-me"/> Keep me signed in
+                <input type="checkbox" onChange={e => setKeepUserSigned(e.target.checked)}/> Keep me signed in
               </label>
             </div>
-            <button className="w-100 btn btn-lg btn-primary" onClick={() => setLoggedIn(true)}>Sign in</button>
+            <button {displayState.loginButtonEnabled? `disabled="true"` : ``} className="w-100 btn btn-lg btn-primary" onClick={() => setLoggedIn(true)}>Sign in</button>
             <a>Don't have an account? Register Here</a>
           </form>
         </main>
@@ -32,6 +52,13 @@ function LoginDisplay({setLoggedIn}) {
       </div>
 
     ); 
-  }
-  export default LoginDisplay;
+}
+export default LoginDisplay;
   
+function validateEmail(email, displayState, setDisplayState){
+    return false;
+}
+
+function authenticate(){
+
+}
