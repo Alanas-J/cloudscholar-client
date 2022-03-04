fe1b96e347e6ae22dbd81356f498ce197caeeb3e
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-03-04 17:43:27 +0000

    saving loginpage progress

--- src/components/login_display/LoginDisplay.jsx
+++ src/components/login_display/LoginDisplay.jsx
@@ -1,57 +1,86 @@
 import { useState } from 'react';
-import './LoginDisplay.css'
+import styles from  './LoginDisplay.module.css'
 
 function LoginDisplay({setLoggedIn}) {
 
     const [displayState, setDisplayState] = useState({
         loginButtonEnabled: true,
-        loginButtonText: "Sign in",
-        error: false,
-        errorMessage: "",
+        error: true,
+        errorMessage: "Auth failure error goes here.",
         submitted: false
     });
-
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
     const [keepUserSigned, setKeepUserSigned] = useState(false);
 
+
+    // State machine.
     if(displayState.submitted){
+
         if(validateEmail(email, displayState, setDisplayState)){
-            authenticate(email, password, setDisplayState)
+
+            if(authenticate(email, password, setDisplayState)){
+
+                if(keepUserSigned){
+                    
+                    // Save login details
+                }
+
+                // Swap view
+            }
         }
     }
 
     return (
-      <div className='loginDisplay text-center'>
-       
-       <main className="form-signin d-flex flex-column justify-content-center">
-          <form className='pb-5'>
-          <img className="mb-4" src={require('./logo192.png')} alt="" width="72" height="57"/>
-            <h1 className="h3 mb-3 fw-normal">CloudScholar</h1>
+        <div className={styles.loginDisplay+' '}>
+            <main className={styles.formSignin+' d-flex flex-column justify-content-center'}>
+                <form className='pb-5'>
 
-            <div className="form-floating my-1">
-              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" onChange={e => setEmail(e.target.value)}/>
-              <label htmlFor="floatingInput">Email address</label>
-            </div>
-            <div className="form-floating my-1">
-              <input type="password" className="form-control" id="floatingPassword" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
-              <label htmlFor="floatingPassword">Password</label>
-            </div>
+                    <div className="logo text-center">
+                        <img className="mb-4 text-center" src={require('./logo192.png')} alt="" width="72" height="57"/>
+                    </div>
+                    
+                    <h1 className="h3 mb-3 fw-normal text-center">CloudScholar</h1>
 
-            <div className="checkbox mb-3">
-              <label>
-                <input type="checkbox" onChange={e => setKeepUserSigned(e.target.checked)}/> Keep me signed in
-              </label>
-            </div>
-            <button {displayState.loginButtonEnabled? `disabled="true"` : ``} className="w-100 btn btn-lg btn-primary" onClick={() => setLoggedIn(true)}>Sign in</button>
-            <a>Don't have an account? Register Here</a>
-          </form>
-        </main>
-        
+                    <div className="auth-error-div">
+                        <div class="alert alert-danger mb-1" role="alert">
+                            {displayState.errorMessage}
+                        </div>
+                    </div>
 
-      </div>
+                    <div className="login-inputs">
+                        <div className="form-floating my-1">
+                            <input type="email" className="form-control is-invalid" id="floatingInput" placeholder="name@example.com" onChange={e => setEmail(e.target.value)}/>
+                            <label htmlFor="floatingInput">Email address</label>
+                            <div id="validationServerUsernameFeedback" className="text-left invalid-feedback mb-2 ms-2">
+                                Please enter valid email. Eg. 'person@mail.com'.
+                            </div>
+                            
+                        </div>
 
-    ); 
+                        
+                        <div className="form-floating my-1">
+                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                        <div className="checkbox mb-3 text-center">
+                        <label >
+                            <input type="checkbox" onChange={e => setKeepUserSigned(e.target.checked)}/> Keep me signed in
+                        </label>
+                        </div>
+                        <button disabled={!displayState.loginButtonEnabled} className="w-100 btn btn-lg btn-primary" 
+                            onClick={(e) => {setDisplayState({...displayState, submitted: true}); e.preventDefault();}}>
+                            {displayState.loginButtonEnabled? "Sign in" : "Signing in..."}
+                        </button>
+                        <div className='text-center'>
+                            <a>Don't have an account? Register Here</a>
+                        </div>
+                        
+                    </div>
+                    
+                </form>
+            </main>
+        </div>); 
 }
 export default LoginDisplay;
   
