7f4e99bf871fba0db170d5ec5a816310523aeea2
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-03-23 15:50:43 +0000

    working tasks display

--- src/components/main_display/home/upcoming_tasks/UpcomingTasksDisplay.jsx
+++ src/components/main_display/home/upcoming_tasks/UpcomingTasksDisplay.jsx
@@ -11,11 +11,7 @@ function UpcomingTasksDisplay() {
 
     return (
         <div className={styles.display + " mt-4 border bg-light border-secondary"}>
-                <Task colour='#80003A' taskName='Interrim Demo' time='24h' subject='Fourth Year Project' />
-                <Task colour='#80AA3A' taskName='Assignment 1' time='24/2/22' subject='Another Module Placeholder' />
-
-                {tasks? tasks.map(task => {return <Task task='task'/>}) : 'nothing' }
-
+                {tasks.length !== 0? tasks.map(task => {return <Task task={task}/>}) : <div className='h5 h-100 p-3 m-0 '> You're currently clear of any tasks! Do you have any to add? </div>}
         </div>
     );
 }   
@@ -27,14 +23,17 @@ function getTasks(userData) {
 
     // merge all subject tasks into one array and keep subject name and colour.
     if (userData.subjects){
-        return userData.subjects.reduce((tasks, subject) => {
+        const taskList = userData.subjects.reduce((tasks, subject) => {
             
             if(subject.tasks){
                 for(let task of subject.tasks){
 
+                    if(task.completed)
+                        break;
+
                     const standaloneTask = {
                         name: task.name,
-                        due_time: task.due_datetime,
+                        due_time: new Date(task.due_datetime),
                         colour: subject.colour,
                         subjectName: subject.name,
                         description: task.description,
@@ -46,6 +45,8 @@ function getTasks(userData) {
             return tasks;
 
         }, []);
+
+        return taskList.sort((task_a, task_b) => task_a.due_time.getTime() > task_b.due_time.getTime());
 
     } else {
         return [];
--- src/components/main_display/home/upcoming_tasks/task/Task.jsx
+++ src/components/main_display/home/upcoming_tasks/task/Task.jsx
@@ -1,26 +1,26 @@
 import styles from './Task.module.css'
 
-function Task({colour, taskName, time, subject}) {
+function Task({task}) {
 
-
+    
 
 
     return (
         <div className={ styles.task+" border border-secondary m-1 rounded d-flex "}>
-            <div className={"rounded-start me-2"} style={{width:'.45rem', backgroundColor: colour}}></div>
+            <div className={"rounded-start me-2"} style={{width:'.3rem', backgroundColor:task.colour}}></div>
 
             <div className={styles.taskContent}>
                 <div className="d-flex justify-content-between">
                     <div className={styles.taskTitleWidth+" pt-1"}>
-                        <h5 className={styles.textOverflow + " border-bottom border-secondary p-1"}>{taskName}</h5>
+                        <h5 className={styles.textOverflow + " border-bottom border-secondary p-1"}>{task.name}</h5>
                     </div>
-                    <div className="me-3 ">[*]</div>
+                    <div className="me-3 "></div>
                 </div>
                 
                 <div className="w-100">
                     <div>
                         <p className={styles.textOverflow + " m-0"}>
-                        Due {time} - {subject}</p>
+                        Due {dueDateToString(task.due_time)} - {task.subjectName}</p>
                     </div>
                     
                 </div>
@@ -29,5 +29,13 @@ function Task({colour, taskName, time, subject}) {
         </div>
     );
 }   
-
 export default Task;
+
+// TODO: make a nice duetime string format.
+function dueDateToString(due_time){
+
+    const date = new Date(due_time)
+
+
+    return date.toUTCString();
+}
