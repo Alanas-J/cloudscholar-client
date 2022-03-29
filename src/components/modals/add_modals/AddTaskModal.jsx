4f0e29b4b9354608c74a298d281f59bbda795e73
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-03-29 11:04:28 +0100

    add task progress checkpoint, gui setup, logic implementation start

--- src/components/modals/add_modals/AddTaskModal.jsx
+++ src/components/modals/add_modals/AddTaskModal.jsx
@@ -1,71 +1,179 @@
 import {Modal, Button} from 'react-bootstrap';
+import {useEffect, useState} from 'react';
 import styles from '../Modal.module.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {openModal} from '../../../state/slices/modalState';
+import {DateTime} from 'luxon';
+import addClassToSubject from '../../../utility/user_data/addClassToSubject';
 
-// Redux state
-import {useDispatch} from 'react-redux';
-import {openModal} from '../../../state/slices/modalState'
 
-function AddTaskModal({show, handleClose}) {
+function AddClassModal({show, handleClose}) {
+
+    // State
     const dispatch = useDispatch();
+    const userData = useSelector(state => state.userState.value.userData);
+
+
+    // Form inputs
+    const [subject, setSubject] = useState(null);
+    const [due_time, setDue_time] = useState(null);
+    const [due_date, setDue_date] = useState(null);
+    const [type, setType] = useState(null);
+    const [location, setLocation] = useState(null);
+    const [description, setDescription] = useState(null);
+
+    const [formState, setFormState] = useState({
+        submissionAttempted: false,
+        submitted: false
+    });
+
+    // Validation
+    const subjectSelected = !!subject;
+    const validTime = validateTime(due_time);
+    const validDate = validateDate(due_date);
+    const inputsAreValid =  subjectSelected && validTime && validDate;
+
+    useEffect(() => {
+        if(formState.submitted){
+            const taskObject = { due_time, due_date, type, location, description};
+
+            setFormState({
+                submissionAttempted: false,
+                submitted: false
+            });
+
+            handleClassAdd(subject, taskObject, setFormState, userData, dispatch);
+        
+        }
+    }, [formState.submitted, subject, due_time, due_date, type, location, description, userData, dispatch]);
+
 
     return (
-        <Modal show={show} onHide={handleClose} centered>
+        <Modal show={show} onHide={!formState.submitted? handleClose : undefined} centered>
             <Modal.Header closeButton>
                 <Modal.Title>Add a Task</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                
                 <form>
-                    <div className="form-group py-2">
-                        <label htmlFor="exampleFormControlSelect1">What Subject is this task for?</label>
-                        <select className="form-control" id="exampleFormControlSelect1">
-                        <option></option>
-                        <option>Sample module 1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
+                    <div className="form-group pt-2">
+                        <label>What Subject is this task for?</label>
+                        <select defaultValue="-Select Subject-" className={((!subjectSelected && formState.submissionAttempted) && "is-invalid") + " form-control form-select"}  onChange={e => {setSubject(e.target.value); setFormState({...formState, success: false})}}>
+                            <option disabled>-Select Subject-</option>
+                            {userData.subjects && userData.subjects.map((subject, index) => {
+                                return (<option key={index}>{subject.name}</option>)})}
                         </select>
+                        <div className="text-left invalid-feedback  ms-2">
+                            Please select a subject.
+                        </div>
                         <div className="row px-3">
-                            <a className={styles.pointable +" link-primary"} onClick={() => dispatch(openModal('AddSubject'))}>
+                            <p className={styles.pointable +" link-primary"} onClick={() => dispatch(openModal('AddSubject'))}>
                                 Add new subjects here +
-                            </a>
+                            </p>
                         </div>
                     </div>
 
                     <div className="form-group pt-2">
-                        <label htmlFor="exampleFormControlSelect1">When is the Task due?</label>
-                        
-                        <div className="row">
+                        <label>When is the task due?</label>
+                        <div className="row pt-2">
                             <div className="col">
-                                <input type="text" className="form-control" placeholder="Time"/>
+                                <input type="text" className={((!validTime && formState.submissionAttempted) && "is-invalid") + " form-control"} placeholder="Time eg. 12:00" onChange={e => {setDue_time(e.target.value); setFormState({...formState, success: false})}}/>
+                                <div className="text-left invalid-feedback  ms-2">
+                                    Expecting 'hh:mm'.
+                                </div>
                             </div>
-
+                            
                             <div className="col">
-                            <input type="text" className="form-control" placeholder="Date"/>
+                                <input type="text" className={((!validDate && formState.submissionAttempted) && "is-invalid") + " form-control"} placeholder="Date eg. 01/12/2022" onChange={e => {setDue_date(e.target.value); setFormState({...formState, success: false})}}/>
+                                <div className="text-left invalid-feedback  ms-2">
+                                    Expecting 'dd/mm/yyyy'.
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <div className="form-group py-2">
-                        
                             <div className="form-group pt-4 pb-2">
-                            <label htmlFor="exampleFormControlTextarea1">Description</label>
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Describe the task/assignment/meeting here..."></textarea>
+                            <label>Description</label>
+                            <textarea className="form-control" rows="3" placeholder="Any additional information... (Optional)"  onChange={e => {setDescription(e.target.value); setFormState({...formState, success: false})}}></textarea>
                         </div>
                     </div>
-    
                 </form>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
+                <div className="col-12 px-3 text center"> 
+                    {formState.errorMessage &&
+                        <div className="alert alert-danger" role="alert">
+                            Error: {formState.errorMessage}
+                        </div>
+                    }
+                    {formState.success &&
+                        <div className="alert alert-success" role="alert">
+                            Task added!
+                        </div>}    
+                </div>
+                <Button variant="secondary" onClick={!formState.submitted? handleClose : undefined}>
                 Close
                 </Button>
-                <Button variant="primary" onClick={handleClose}>
+                <Button variant="primary" disabled={formState.submitted || formState.success} onClick={() => handleSubmit(setFormState, inputsAreValid)}>
                 Add Class
                 </Button>
             </Modal.Footer>
         </Modal>
     ); 
-  }
-  export default AddTaskModal;
+}
+export default AddClassModal;
   
+
+function validateTime(time){
+    if(/^([0-1]?\d|2[0-3])(?::([0-5]\d))$/.test(time))
+        return DateTime.fromISO(time).isValid();
+
+    return false;
+}
+
+function validateDate(time){
+    return  /^([0-1]?\d|2[0-3])(?::([0-5]\d))$/.test(time);
+}
+
+
+
+function handleSubmit(setFormState, inputsAreValid){
+    setFormState({
+        submissionAttempted: true,
+        submitted: inputsAreValid
+    });
+}
+
+
+async function handleClassAdd(subjectName, taskObject, setFormState, userData, dispatch){
+    /*
+
+    const newUserData = addClassToSubject(subjectName, taskObject, userData);
+    const formState = {
+        submissionAttempted: false,
+        submitted: false
+    };
+
+    try {
+        await updue_dateUserData(newUserData, dispatch);
+
+        formState.success = true;
+        formState.errorMessage = false;
+        formState.clearInputs = true;
+
+    } catch (error) {
+
+        if(error.message === 'Network Error'){
+            formState.errorMessage = "Connection to the server failed, if problem persists, restart the application.";
+        } else if (error.response.data.message){
+            formState.errorMessage = error.response.data.message;
+        } else{
+            formState.errorMessage = 'An unknown error has occured.'
+        }
+        formState.success = false;
+        console.log({error: error});
+
+    }
+    setFormState(formState);
+    */
+}
