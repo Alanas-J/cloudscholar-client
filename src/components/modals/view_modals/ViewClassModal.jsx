0971d0a16e3814fa49ba3af117e3258fbf69c436
Alanas-J <alanasjakubauskas@gmail.com>
2022-04-01 10:21:46 +0100

    view class deletion skeleton created, need a delete classFromuserdata method and then just post update

--- src/components/modals/view_modals/ViewClassModal.jsx
+++ src/components/modals/view_modals/ViewClassModal.jsx
@@ -1,9 +1,26 @@
 import {Modal, Button} from 'react-bootstrap';
 import styles from '../Modal.module.css'
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
 
 function ViewClassModal({show, data, handleClose}) {
+
+    const dispatch = useDispatch();
+    const userData = useSelector(state => state.userState.value.userData);
+    const [modalState, setModalState] = useState({
+        deleteClicked: false,
+        deleting: false,
+        deleted: false,
+        error: null
+    });
+
+
+    useEffect(() => {
+        if(modalState.deleting){
+            handleDelete(data, userData, setModalState, dispatch);
+        }
+    })
 
     
     return (
@@ -13,37 +30,41 @@ function ViewClassModal({show, data, handleClose}) {
                     <div className="display-5 px-3">Class View</div>
                 </div>
             </Modal.Header>
+
             <Modal.Body>
-
-            <div className="mb-4">
-                <div className="mx-2 h5 border-bottom">Belongs to</div>
-                <div className="d-flex px-5"><div className="pe-2">{data.subject.name}</div><div className={styles.colourDiv+ ' border'} style={{backgroundColor: data.subject.colour}}></div></div>
-            </div>
-
-            <div className="my-3">
-                <div className="mx-2 h5 border-bottom">Class Details</div>
-
-                <div className="px-5 pb-1">Class Type: {data.type}</div>
-                <div className="px-5 pb-1">Day: {dayIntToString(data.day)}</div>
-                <div className="px-5 pb-1">Duration: {data.start_time} - {data.end_time}</div>
-                <div className="px-5 font-weight-bold pb-1">Description: </div>
-                <div className='px-4 mx-5 rounded py-1' style={{whiteSpace: 'pre-wrap', backgroundColor: '#2222220F'}}>
-
-
-                    {data.description? data.description : 'No description provided'}
+                <div className="mb-4">
+                    <div className="mx-2 h5 border-bottom">Belongs to</div>
+                    <div className="d-flex px-5"><div className="pe-2">{data.subject.name}</div><div className={styles.colourDiv+ ' border'} style={{backgroundColor: data.subject.colour}}></div></div>
                 </div>
-                
-            </div>
 
+                <div className="my-3">
+                    <div className="mx-2 h5 border-bottom">Class Details</div>
+                    <div className="px-5 pb-1">Class Type: {data.type}</div>
+                    <div className="px-5 pb-1">Day: {dayIntToString(data.day)}</div>
+                    <div className="px-5 pb-1">Duration: {data.start_time} - {data.end_time}</div>
+                    <div className="px-5 font-weight-bold pb-1">Description: </div>
+                    <div className='px-4 mx-5 rounded py-1' style={{whiteSpace: 'pre-wrap', backgroundColor: '#2222220F'}}>
+                        {data.description? data.description : 'No description provided'}
+                    </div> 
+                </div>
             </Modal.Body>
 
-            <Modal.Footer className='border-top-0 px-4 mx-54'>
-
+            <Modal.Footer className='border-top-0 px-4 mx-4'>
+                <div className="col-12 px-3 text center"> 
+                    {modalState.error &&
+                        <div className="alert alert-danger" role="alert">
+                            Error: {modalState.error}
+                        </div>
+                    }
+                </div>
                 <Button variant="secondary" onClick={handleClose}>
                     Close
                 </Button>
-                <Button variant="danger" >
-                    Delete Class
+                <Button disable={(modalState.deleted || modalState.deleting)} onClick={() => handleDeleteClick(modalState, setModalState)} variant="danger" >
+                    {!modalState.deleteClicked && 'Delete Class'} 
+                    {modalState.deleteClicked && 'Warning! Click to confirm deletion'}
+                    {modalState.deleting && 'Deleting class...'}
+                    {modalState.deleted && 'Class deleted'}
                 </Button>
             </Modal.Footer>
         </Modal>
@@ -51,6 +72,44 @@ function ViewClassModal({show, data, handleClose}) {
 }
 export default ViewClassModal;
   
+
+function handleDeleteClick(modalState, setModalState){
+
+    if(!modalState.deleteClicked){
+        setModalState({
+            deleteClicked: true,
+            deleting: false,
+            deleted: false,
+            error: null
+        });
+
+    } else if(modalState.deleteClicked){
+        setModalState({
+            deleteClicked: false,
+            deleting: true,
+            deleted: false,
+            error: null
+        });
+    }
+}
+
+
+async function handleDelete(_class, userData, setModalState, dispatch){
+
+    // get userdata with deleted class
+    // set a modalstate payload.
+
+    try {
+        // try update userdata
+
+        // dispatch new userData
+        // set modal state
+    } catch (error) {
+        
+        // set error state
+    }
+}
+
 
 function dayIntToString(int){
 
--- src/components/modals/view_modals/ViewTaskModal.jsx
+++ src/components/modals/view_modals/ViewTaskModal.jsx
@@ -51,29 +51,3 @@ export default ViewTaskModal;
 function handleDelete(){
 
 }
-  
-
-function dayIntToString(int){
-
-    switch(int){
-        case 1:
-            return 'Monday';
-        case 2:
-            return 'Tuesday';
-        case 3:
-            return 'Wednesday';
-        case 4:
-            return 'Thursday';
-        case 5:
-            return 'Friday';
-        case 6:
-            return 'Saturday';
-        case 7:
-            return 'Sunday';
-        default:
-            return 'n/a'
-
-    }
-
-
-}
