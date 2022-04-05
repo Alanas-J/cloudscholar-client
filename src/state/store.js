40af49d46e4c47b380c0f5a143c3af53272725c0
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-04-05 17:03:58 +0100

    state store updated

--- src/state/store.js
+++ src/state/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './slices/counterSlice';
 import displayReducer from './slices/appDisplay';
 import modalReducer from './slices/modalState';
 import userStateReducer from './slices/userState'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     appDisplay: displayReducer,
     modalState: modalReducer,
     userState: userStateReducer
